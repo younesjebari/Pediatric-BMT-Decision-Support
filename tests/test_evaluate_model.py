8368f409549f5562488c3be38d8b181e7358a3cb
Aroua <starwa64@gmail.com>
2026-03-15 13:03:33 +0000

    Update test_evaluate_model.py

--- tests/test_evaluate_model.py
+++ tests/test_evaluate_model.py
@@ -7,7 +7,7 @@ import joblib
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from data_processing import IMPORTANT_FEATURES
+from src.data_processing import IMPORTANT_FEATURES
 
 
 @pytest.fixture
